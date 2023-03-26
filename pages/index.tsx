1e0b543a43edbea2d985abecf542e2b9e89898bd
Mike Belyayev <4xgood@gmail.com>
2023-03-25 21:15:38 -0600

    findMany commented out

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,59 +1,60 @@
-// import React from "react";
-// import { GetStaticProps } from "next";
-// import Layout from "../components/Layout";
-// import Post, { PostProps } from "../components/Post";
-// import prisma from "../lib/prisma";
-// import Header from "./header";
+import React from "react";
+import { GetStaticProps } from "next";
+import Layout from "../components/Layout";
+import Post, { PostProps } from "../components/Post";
+import prisma from "../lib/prisma";
+import Header from "./header";
 
-// export const getStaticProps: GetStaticProps = async () => {
-//   const feed = await prisma.post.findMany({
-//     where: { published: true },
-//     include: {
-//       author: {
-//         select: { name: true },
-//       },
-//     },
-//   });
-//   return {
-//     props: { feed },
-//     revalidate: 10,
-//   };
-// };
+export const getStaticProps: GetStaticProps = async () => {
+  // const feed = await prisma.post.findMany({
+  //   where: { published: true },
+  //   include: {
+  //     author: {
+  //       select: { name: true },
+  //     },
+  //   },
+  // });
+  return {
+    // props: { feed },
+    props: {},
+    revalidate: 10,
+  };
+};
 
-// type Props = {
-//   feed: PostProps[];
-// };
+type Props = {
+  feed: PostProps[];
+};
 
-// const Blog: React.FC<Props> = (props) => {
-//   return (
-//     <Layout>
-//       <Header />
-//       <div className="page">
-//         <h1>Public Feed</h1>
-//         <main>
-//           {props.feed.map((post) => (
-//             <div key={post.id} className="post">
-//               <Post post={post} />
-//             </div>
-//           ))}
-//         </main>
-//       </div>
-//       <style jsx>{`
-//         .post {
-//           background: white;
-//           transition: box-shadow 0.1s ease-in;
-//         }
+const Blog: React.FC<Props> = (props) => {
+  return (
+    <Layout>
+      <Header />
+      <div className="page">
+        <h1>Public Feed</h1>
+        <main>
+          {props.feed.map((post) => (
+            <div key={post.id} className="post">
+              <Post post={post} />
+            </div>
+          ))}
+        </main>
+      </div>
+      <style jsx>{`
+        .post {
+          background: white;
+          transition: box-shadow 0.1s ease-in;
+        }
 
-//         .post:hover {
-//           box-shadow: 1px 1px 3px #aaa;
-//         }
+        .post:hover {
+          box-shadow: 1px 1px 3px #aaa;
+        }
 
-//         .post + .post {
-//           margin-top: 2rem;
-//         }
-//       `}</style>
-//     </Layout>
-//   );
-// };
+        .post + .post {
+          margin-top: 2rem;
+        }
+      `}</style>
+    </Layout>
+  );
+};
 
-// export default Blog;
+export default Blog;
