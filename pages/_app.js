0f51e2fb395f5d1c8c092823d0a1bbc10eacbf55
Mike Belyayev <4xgood@gmail.com>
2023-03-29 12:07:41 -0600

    testing styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,7 @@
 import { Provider } from "next-auth/client";
 
 import Layout from "../components/layout/layout";
-// import '../styles/globals.css';
-
+import styles from "./App.module.scss";
 function MyApp({ Component, pageProps }) {
   return (
     <Provider session={pageProps.session}>
