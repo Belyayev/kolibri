352b1c89cebcd6aedcf0cf8f7dec98ed49c1a549
Mike Belyayev <4xgood@gmail.com>
2023-06-03 16:00:34 -0600

    added a modal

--- components/books/bookItem.js
+++ components/books/bookItem.js
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import Image from "next/image";
 import classes from "./books.module.css";
+import { Button, Modal } from "antd";
 
 function BookItem({ props }) {
   const {
@@ -18,31 +20,85 @@ function BookItem({ props }) {
     link =
       "https://github.com/Belyayev/kolibri/blob/main/Images/NoImage.png?raw=true";
   }
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <div className={classes.bookitem}>
-      <div className={classes.bookTitle}>{bookName}</div>
-      <Image
-        src={link}
-        alt=""
-        title=""
-        width="100%"
-        height="100%"
-        layout="responsive"
-        objectFit="contain"
-      />
-      {bookAuthor && (
-        <div className={classes.bookAuthor}>Автор: {bookAuthor}</div>
-      )}
-      <div className={classes.bookDescription}>{bookDescription}</div>
-      {bookHolder ? (
-        <div className={classes.notAvailable}>Книга на руках</div>
-      ) : (
-        <div className={classes.available}>Книга доступна</div>
-      )}
-      {numberOfPages && (
-        <div className={classes.pages}>Страниц: {numberOfPages}</div>
-      )}
-    </div>
+    <>
+      <Modal
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={[
+          <Button key="back" onClick={handleCancel}>
+            Закрыть
+          </Button>,
+        ]}
+      >
+        <div className={classes.bookitem}>
+          <div className={classes.bookTitle}>{bookName}</div>
+          <Image
+            onClick={showModal}
+            src={link}
+            alt=""
+            title=""
+            width="100%"
+            height="100%"
+            layout="responsive"
+            objectFit="contain"
+            style={{ cursor: "pointer" }}
+          />
+          {bookAuthor && (
+            <div className={classes.bookAuthor}>Автор: {bookAuthor}</div>
+          )}
+          <div className={classes.bookDescription}>{bookDescription}</div>
+          {bookHolder ? (
+            <div className={classes.notAvailable}>Книга на руках</div>
+          ) : (
+            <div className={classes.available}>Книга доступна</div>
+          )}
+          {numberOfPages && (
+            <div className={classes.pages}>Страниц: {numberOfPages}</div>
+          )}
+        </div>
+      </Modal>
+      <div className={classes.bookitem}>
+        <div className={classes.bookTitle}>{bookName}</div>
+        <Image
+          onClick={showModal}
+          src={link}
+          alt=""
+          title=""
+          width="100%"
+          height="100%"
+          layout="responsive"
+          objectFit="contain"
+          style={{ cursor: "pointer" }}
+        />
+        {bookAuthor && (
+          <div className={classes.bookAuthor}>Автор: {bookAuthor}</div>
+        )}
+        <div className={classes.bookDescription}>{bookDescription}</div>
+        {bookHolder ? (
+          <div className={classes.notAvailable}>Книга на руках</div>
+        ) : (
+          <div className={classes.available}>Книга доступна</div>
+        )}
+        {numberOfPages && (
+          <div className={classes.pages}>Страниц: {numberOfPages}</div>
+        )}
+      </div>
+    </>
   );
 }
 
